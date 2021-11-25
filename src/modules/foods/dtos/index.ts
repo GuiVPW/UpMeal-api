f28c6ab8ec580eaef94ae5500c2045256e6ef9c3
Guilherme Vieira <guivpw68@gmail.com>
2021-11-25 01:03:48 -0300

    feat: add update food dto

---
+++ src/modules/foods/dtos/index.ts
@@ -0,0 +1,3 @@
+export * from './create.dto'
+export * from './find-many.dto'
+export * from './update.dto'
---
+++ src/modules/foods/dtos/update.dto.ts
@@ -0,0 +1,15 @@
+import { IsDateString, IsNumber, IsOptional, IsString } from 'class-validator'
+
+export class UpdateDto {
+	@IsString()
+	@IsOptional()
+	name?: string
+
+	@IsNumber({ maxDecimalPlaces: 1 })
+	@IsOptional()
+	quantity?: number
+
+	@IsDateString()
+	@IsOptional()
+	validity?: string
+}
