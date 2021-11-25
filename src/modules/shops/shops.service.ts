57302148b51c9f8f241f456982b7abb71410c0ca
Guilherme Vieira <guivpw68@gmail.com>
2021-11-25 00:01:13 -0300

    feat: add shop service

---
+++ src/modules/shops/shops.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common'
+
+import { SignUpDto, LoginDto, FindOneDto, FindManyDto } from './dtos'
+import {
+	SignUpUseCase,
+	LoginUseCase,
+	FindByIdUseCase,
+	FindOneUseCase,
+	FindManyUseCase
+} from './use-cases'
+
+@Injectable()
+export class ShopService {
+	constructor(
+		private signUpUseCase: SignUpUseCase,
+		private loginUseCase: LoginUseCase,
+		private findByIdUseCase: FindByIdUseCase,
+		private findOneUseCase: FindOneUseCase,
+		private findManyUseCase: FindManyUseCase
+	) {}
+
+	async signUp(input: SignUpDto) {
+		return this.signUpUseCase.execute(input)
+	}
+
+	async login(input: LoginDto) {
+		return this.loginUseCase.execute(input)
+	}
+
+	async findById(id: number) {
+		return this.findByIdUseCase.execute(id)
+	}
+
+	async findOne(fields: FindOneDto) {
+		return this.findOneUseCase.execute(fields)
+	}
+
+	async findMany(fields: FindManyDto) {
+		return this.findManyUseCase.execute(fields)
+	}
+}
