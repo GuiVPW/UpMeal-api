c1d2c5e642289093061c8f9ac700cd8a8dc641d6
Guilherme Vieira <guivpw68@gmail.com>
2021-11-24 23:59:24 -0300

    feat: add find many use case

---
+++ src/modules/shops/use-cases/find-many/find-many.use-case.ts
@@ -0,0 +1,44 @@
+import { BaseUseCase } from '@common/domain/base'
+import { formatPhone } from '@common/utils'
+import { FindManyDto } from '@modules/shops/dtos'
+import { Shop } from '@modules/shops/entities'
+import { Injectable, Logger } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { ILike, Repository } from 'typeorm'
+
+@Injectable()
+export class FindManyUseCase implements BaseUseCase<Shop> {
+	private logger: Logger = new Logger('FindManySpecialty')
+
+	constructor(
+		@InjectRepository(Shop)
+		private readonly shopRepository: Repository<Shop>
+	) {}
+
+	async execute(where: FindManyDto): Promise<{ shops: Shop[] }> {
+		const keys: Partial<Record<keyof Shop, unknown>> = {}
+
+		if (where.name) {
+			this.logger.log('Adding name query with insensitive case and starts with method')
+			keys.name = ILike(`${where.name}%`)
+		}
+
+		const foundShops = await this.shopRepository.find({
+			cache: true,
+			where: keys
+		})
+
+		const formattedShops = foundShops.map(shop => {
+			const { phone, phoneDigits, password, ...fields } = shop
+
+			return {
+				...fields,
+				phone: formatPhone(phoneDigits as number, phone as number)
+			}
+		})
+
+		return {
+			shops: formattedShops
+		}
+	}
+}
