bce8a7639d1c17bb356edae4016b186675f43584
Guilherme Vieira <guivpw68@gmail.com>
2021-11-25 01:31:28 -0300

    feat: add clients controller

---
+++ src/modules/clients/clients.controller.ts
@@ -0,0 +1,42 @@
+import { AccessTokenGuard } from '@common/guards'
+import { Controller, Get, Post, Body, Param, UseGuards, HttpCode } from '@nestjs/common'
+
+import { ClientService } from './clients.service'
+import { Client } from './decorators'
+import { LoginDto, SignUpDto } from './dtos'
+
+@Controller('clients')
+export class clientsController {
+	constructor(private readonly clientService: ClientService) {}
+
+	@Post('signup')
+	@HttpCode(201)
+	async signUp(@Body() input: SignUpDto) {
+		return this.clientService.signUp(input)
+	}
+
+	@Post('login')
+	async login(@Body() input: LoginDto) {
+		return this.clientService.login(input)
+	}
+
+	@UseGuards(AccessTokenGuard)
+	@Get('me')
+	async me(@Client('id') id: number) {
+		const data = await this.clientService.findById(id)
+
+		return {
+			me: data
+		}
+	}
+
+	@UseGuards(AccessTokenGuard)
+	@Get(':id')
+	async findById(@Param('id') id: string) {
+		const data = await this.clientService.findById(+id)
+
+		return {
+			client: data
+		}
+	}
+}
