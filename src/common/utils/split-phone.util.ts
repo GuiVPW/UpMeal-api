55d182f1a6b4f972224f6fdbaa39344f7a6b6e84
Guilherme Vieira <guivpw68@gmail.com>
2021-11-24 23:57:43 -0300

    fix: add type condition

--- src/common/utils/split-phone.util.ts
+++ src/common/utils/split-phone.util.ts
@@ -1,5 +1,10 @@
-export function splitPhone(phone: number): [number, number] {
-	const stringifiedPhone = String(phone)
+export function splitPhone(phone: number | string): [number, number] {
+	let stringifiedPhone = phone
+
+	if (typeof stringifiedPhone === 'number') {
+		stringifiedPhone = String(phone)
+	}
+
 	const dddPhone = +stringifiedPhone.substring(0, 2)
 	const fullPhone = +stringifiedPhone.substring(2, stringifiedPhone.length)
 
