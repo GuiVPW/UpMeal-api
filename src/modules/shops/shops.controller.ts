1341139c8eb9a9c5ff35ecd691e60ba4dd8ac524
Guilherme Vieira <guivpw68@gmail.com>
2021-11-25 00:05:09 -0300

    feat: add shops controller with routes

---
+++ src/modules/shops/shops.controller.ts
@@ -0,0 +1,50 @@
+import { BasicAuthGuard, AccessTokenGuard } from '@common/guards'
+import { Controller, Get, Post, Body, Param, Query, UseGuards } from '@nestjs/common'
+
+import { Shop } from './decorators'
+import { FindManyDto, LoginDto, SignUpDto } from './dtos'
+import { ShopService } from './shops.service'
+
+@Controller('shops')
+export class shopsController {
+	constructor(private readonly shopService: ShopService) {}
+
+	@Post('signup')
+	async signUp(@Body() input: SignUpDto) {
+		return this.shopService.signUp(input)
+	}
+
+	@Post('login')
+	async login(@Body() input: LoginDto) {
+		return this.shopService.login(input)
+	}
+
+	@UseGuards(BasicAuthGuard)
+	@Get('me')
+	async me(@Shop('id') id: number) {
+		const data = await this.shopService.findById(id)
+
+		return {
+			me: data
+		}
+	}
+
+	@Get(':id')
+	async findById(@Param('id') id: string) {
+		const data = await this.shopService.findById(+id)
+
+		return {
+			shop: data
+		}
+	}
+
+	@UseGuards(AccessTokenGuard)
+	@Get()
+	async findMany(@Query() queries: FindManyDto) {
+		const data = await this.shopService.findMany(queries)
+
+		return {
+			shops: data
+		}
+	}
+}
