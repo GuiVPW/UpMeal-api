8171a14def5ec35ed3438ef5d9a3f5f7221e6701
Guilherme Vieira <guivpw68@gmail.com>
2021-11-25 02:32:53 -0300

    fix: make is available type number and add default value to 1

--- src/modules/foods/entities/food.entity.ts
+++ src/modules/foods/entities/food.entity.ts
@@ -9,8 +9,8 @@ export class Food {
 	@Column({ name: 'NM_ALIMENTO', type: 'varchar2', length: 50 })
 	name!: string
 
-	@Column({ name: 'VL_DISPONIBILIDADE', type: 'number', precision: 1 })
-	isAvailable!: boolean
+	@Column({ name: 'VL_DISPONIBILIDADE', type: 'number', precision: 1, default: 1 })
+	isAvailable!: number
 
 	@Column({ name: 'NR_QUANTIDADE', type: 'number', precision: 6, scale: 2 })
 	quantity!: number
@@ -21,9 +21,10 @@ export class Food {
 	@Column({ name: 'CD_ESTABELECIMENTO', type: 'number', precision: 5 })
 	shopId!: number
 
-	@ManyToOne(() => Shop, shop => shop.foods, { eager: true })
+	@ManyToOne(() => Shop, shop => shop.foods)
 	@JoinColumn({
-		name: 'ALIMENTO_ESTAB'
+		name: 'CD_ESTABELECIMENTO',
+		referencedColumnName: 'id'
 	})
 	shop!: Shop
 }
