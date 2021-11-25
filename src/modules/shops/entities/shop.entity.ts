fc11e95fb557696ec7373b95ebe563fa2a470d36
Guilherme Vieira <guivpw68@gmail.com>
2021-11-25 01:01:47 -0300

    fix: prevent shops to get all foods

--- src/modules/shops/entities/shop.entity.ts
+++ src/modules/shops/entities/shop.entity.ts
@@ -26,7 +26,7 @@ export class Shop extends UserModel {
 	@Column({ name: 'VL_LONGITUDE', type: 'number', scale: 6, precision: 9 })
 	longitude!: number
 
-	@OneToMany(() => Food, food => food.shop, { eager: true })
+	@OneToMany(() => Food, food => food.shop, { eager: false })
 	foods: Food[] = []
 
 	@OneToMany(() => Reservation, reservation => reservation.shop)
