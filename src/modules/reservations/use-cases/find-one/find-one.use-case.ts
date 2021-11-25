72759a5ced4106e2be899ff8d1083df859a21aa9
Guilherme Vieira <guivpw68@gmail.com>
2021-11-25 01:57:45 -0300

    feat: add find one reservation by client and shop ids use case

---
+++ src/modules/reservations/use-cases/find-one/find-one.use-case.ts
@@ -0,0 +1,36 @@
+import { BaseUseCase } from '@common/domain/base'
+import { Reservation } from '@modules/reservations/entities'
+import { Injectable, Logger } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+
+@Injectable()
+export class FindOneUseCase implements BaseUseCase<Reservation> {
+	private logger: Logger = new Logger('FindOneSpecialty')
+
+	constructor(
+		@InjectRepository(Reservation)
+		private readonly reservationRepository: Repository<Reservation>
+	) {}
+
+	async execute(
+		shopId: number,
+		clientId: number
+	): Promise<{ reservation: Reservation | null }> {
+		const foundReservation = await this.reservationRepository.findOne({
+			cache: true,
+			where: { shopId, clientId }
+		})
+
+		if (!foundReservation) {
+			this.logger.log('No reservation was found to client with given id')
+			return {
+				reservation: null
+			}
+		}
+
+		return {
+			reservation: foundReservation
+		}
+	}
+}
