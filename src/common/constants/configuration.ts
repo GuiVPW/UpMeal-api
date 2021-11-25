2bdce89baa2a54e3570e79bc3e635c254d024958
Guilherme Vieira <guivpw68@gmail.com>
2021-11-24 23:18:33 -0300

    feat: add database configuration

---
+++ src/common/constants/configuration.ts
@@ -0,0 +1,22 @@
+import { ConnectionAttributes } from 'oracledb'
+
+type Envs = 'development' | 'production'
+
+const {
+	PORT,
+	NODE_ENV: ProcessEnv,
+	DATABASE_USER,
+	DATABASE_PASS,
+	DATABASE_URI,
+	SECRET = 'test123'
+} = process.env
+
+const NODE_ENV: Envs = (ProcessEnv as Envs) || 'development'
+
+const dbConfig: ConnectionAttributes = {
+	user: DATABASE_USER ?? 'oracle',
+	password: DATABASE_PASS ?? 'oracle',
+	connectionString: DATABASE_URI ?? 'localhost:1521/ORCLCDB.localdomain'
+}
+
+export { NODE_ENV, dbConfig, SECRET, PORT }
