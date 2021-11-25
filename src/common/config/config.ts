7e488af35307600f7aa67d22fbe61d184cabbc62
Guilherme Vieira <guivpw68@gmail.com>
2021-11-24 23:18:15 -0300

    feat: add app and database configurations

---
+++ src/common/config/config.ts
@@ -0,0 +1,29 @@
+import { PORT, dbConfig } from '@common/constants/configuration'
+
+export interface AppConfig {
+	nest: NestConfig
+	database: DatabaseConfig
+}
+
+export interface NestConfig {
+	port: number
+}
+
+export interface DatabaseConfig {
+	username: string
+	password: string
+	uri?: string
+}
+
+const config: AppConfig = {
+	nest: {
+		port: +(PORT as string) ?? 5005
+	},
+	database: {
+		password: dbConfig.password as string,
+		username: dbConfig.user as string,
+		uri: dbConfig.connectionString as string
+	}
+}
+
+export default (): AppConfig => config
