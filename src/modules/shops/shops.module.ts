ccd67912cfe21ed21fc5843e12e0ba6a0e6f0ba0
Guilherme Vieira <guivpw68@gmail.com>
2021-11-26 05:00:54 -0300

    feat: add multer module register limits

--- src/modules/shops/shops.module.ts
+++ src/modules/shops/shops.module.ts
@@ -1,4 +1,5 @@
 import { Global, Module } from '@nestjs/common'
+import { MulterModule } from '@nestjs/platform-express'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { Shop } from './entities'
@@ -8,7 +9,10 @@ import { UseCases } from './use-cases'
 
 @Global()
 @Module({
-	imports: [TypeOrmModule.forFeature([Shop])],
+	imports: [
+		TypeOrmModule.forFeature([Shop]),
+		MulterModule.register({ limits: { fileSize: 2_500_000, files: 1 } })
+	],
 	controllers: [ShopsController],
 	providers: [...UseCases, ShopService],
 	exports: [ShopService, TypeOrmModule]
